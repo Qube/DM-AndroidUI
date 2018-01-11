7cf091126fe369a33b28afd444cc4b48c4ff55d9
Qube <artur_kubek@outlook.com>
2018-01-11 12:17:49 +0100

    Initial commit

---
+++ app/src/main/java/agh/to2/androidui/TableCreatorView.java
@@ -0,0 +1,13 @@
+package agh.to2.androidui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TableCreatorView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table_creator_view);
+    }
+}
